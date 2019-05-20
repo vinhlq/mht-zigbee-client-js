1288c035f46025740f17f06e32c9a2b19068863f
deb <vinhlq@hotmail.com>
2019-05-20 12:32:21 +0700

    0.1.2

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   ServerActionsSocketIO: require('./src/actions/ServerActionsSocketIO'),
   ServerActionsAwsIot: require('./src/actions/ServerActionsAwsIot'),
   ServerActionsAmplifyPubSub: require('./src/actions/ServerActionsAmplifyPubSub'),
+  ServerActions: require('./src/actions/ServerActions'),
   Constants: require('./src/Constants')
 }
